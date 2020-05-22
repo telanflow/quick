1f7abe052f45e5075aa222b2a24db0cb4f46bcb1
telanflow <telanflow@gmail.com>
2020-05-22 17:00:02 +0800

    [fix] MergeQuesyString 修复逻辑bug

--- utils.go
+++ utils.go
@@ -35,7 +35,23 @@ func CopyHeader(h http.Header) http.Header {
 }
 
 // Get request merge url and query string encode.
-func MergeQueryParams(parsedURL *url.URL, parsedQuery string) (*url.URL, error) {
+func MergeQueryString(parsedURL *url.URL, parsedQuery string) (*url.URL, error) {
+	value1 := parsedURL.Query()
+	value2, err := url.ParseQuery(parsedQuery)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range value2 {
+		for _, v2 := range v {
+			value1.Set(k, v2)
+		}
+	}
+	parsedURL.RawQuery = value1.Encode()
+	return parsedURL, nil
+}
+
+// Get request replace url and query string encode.
+func ReplaceQueryString(parsedURL *url.URL, parsedQuery string) (*url.URL, error) {
 	rawurl := strings.Join([]string{strings.Replace(parsedURL.String(), "?"+parsedURL.RawQuery, "", -1), parsedQuery}, "?")
 	u, err := url.Parse(rawurl)
 	if err != nil {
--- utils_test.go
+++ utils_test.go
@@ -1,7 +1,9 @@
 package quick
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -14,12 +16,50 @@ func TestMergeHeaders(t *testing.T) {
 	h2.Set("Server", "nginx")
 
 	h3 := MergeHeaders(h1, h2)
-	if h3.Get("Context-type") != "text/xml" {
-		t.Fail()
-	}
-	if h3.Get("Server") != "nginx" {
-		t.Fail()
-	}
 
-	t.Log(h3)
+	asserts := assert.New(t)
+	asserts.Equal(h3.Get("Context-type"), "text/xml")
+	asserts.Equal(h3.Get("Server"), "nginx")
+}
+
+func TestCopyHeader(t *testing.T) {
+	h1 := make(http.Header)
+	h1.Set("Context-Type", "text/html")
+
+	h2 := CopyHeader(h1)
+
+	asserts := assert.New(t)
+	asserts.Equal(h1.Get("Context-Type"), h2.Get("Context-Type"))
+}
+
+func TestMergeQueryString(t *testing.T) {
+	asserts := assert.New(t)
+
+	u, _ := url.Parse("http://example.com")
+	u2, _ := MergeQueryString(u, "c=3&d=4")
+	asserts.Equal(u2.Query().Encode(), "c=3&d=4")
+
+	u, _ = url.Parse("http://example.com?a=1&b=2")
+	u2, _ = MergeQueryString(u, "c=3&d=4")
+	asserts.Equal(u2.Query().Encode(), "a=1&b=2&c=3&d=4")
+
+	u, _ = url.Parse("http://example.com?a=1")
+	u2, _ = MergeQueryString(u, "a=2")
+	asserts.Equal(u2.Query().Encode(), "a=2")
+
+	u, _ = url.Parse("http://example.com?a=1")
+	u2, _ = MergeQueryString(u, "a=2&a=3")
+	asserts.Equal(u2.Query().Encode(), "a=3")
+}
+
+func TestReplaceQueryString(t *testing.T) {
+	asserts := assert.New(t)
+
+	u, _ := url.Parse("http://example.com")
+	u2, _ := ReplaceQueryString(u, "c=3&d=4")
+	asserts.Equal(u2.Query().Encode(), "c=3&d=4")
+
+	u, _ = url.Parse("http://example.com?a=1")
+	u2, _ = ReplaceQueryString(u, "c=3&d=4")
+	asserts.Equal(u2.Query().Encode(), "c=3&d=4")
 }
